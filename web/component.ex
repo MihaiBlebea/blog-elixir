c0f6e306fafe14d7634cdba09c57336cb341b8be
Mihai Blebea <mihai@getchip.uk>
2020-12-30 14:59:48 +0000

    Adds video to course

--- web/component.ex
+++ web/component.ex
@@ -5,6 +5,7 @@ defmodule Blog.Web.Component do
     EEx.function_from_file :def, :component_navigation, "templates/components/navigation.html.eex"
     EEx.function_from_file :def, :component_main, "templates/components/main.html.eex"
     EEx.function_from_file :def, :component_main_article, "templates/components/main_article.html.eex", [:title, :subtitle, :image]
+    EEx.function_from_file :def, :component_main_article_video, "templates/components/main_article_video.html.eex", [:title, :video]
     EEx.function_from_file :def, :component_head, "templates/components/head.html.eex"
     EEx.function_from_file :def, :component_footer, "templates/components/footer.html.eex"
     EEx.function_from_file :def, :component_divider, "templates/components/divider.html.eex", [:title]
