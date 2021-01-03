6a6b99a777f19e48f08b839257273824099660cf
Mihai Blebea <mihai@getchip.uk>
2021-01-03 15:52:25 +0000

    Removes files that are not used

--- lib/blog.ex
+++ lib/blog.ex
@@ -16,15 +16,6 @@ defmodule Blog do
 
         children = [
             {Plug.Cowboy, scheme: :http, plug: Blog.Web.Router, options: [port: port]},
-            # {
-            #     MyXQL,
-            #     username: Application.get_env(:blog, :mysql_user),
-            #     password: Application.get_env(:blog, :mysql_password),
-            #     hostname: Application.get_env(:blog, :mysql_host),
-            #     port: Application.get_env(:blog, :mysql_port),
-            #     database: Application.get_env(:blog, :mysql_database),
-            #     name: :blog_db
-            # },
             {
                 Blog.ContentMetaRepo, [
                     %{content_meta_url: Application.get_env(:blog, :content_meta_url)}
@@ -32,14 +23,6 @@ defmodule Blog do
             }
         ]
 
-        supervisor = Supervisor.start_link(children, strategy: :one_for_one)
-
-        # migrate_database()
-
-        supervisor
+        Supervisor.start_link(children, strategy: :one_for_one)
     end
-
-    # defp migrate_database() do
-    #     Blog.Repo.Article.create_table()
-    # end
 end
